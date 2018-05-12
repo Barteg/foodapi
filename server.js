0c6dc29bb268b76555f92f9b4a9a84c19780934f
Bartek <bartek.szymanski@interia.eu>
2018-05-12 15:01:09 +0200

    added || 5000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 app.use(require('./controllers'))
 
-var port = process.env.PORT || 3000
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 5000
+var server = app.listen(5000, function () {
   console.log('Server successfully started on port %d', server.address().port)
 })
